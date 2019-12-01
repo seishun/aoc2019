bbd13f86857116654ebd84da3b82ee18f5cb176e
Nikolai Vavilov <vvnicholas@gmail.com>
2019-12-01 17:54:49 +0200

    Add day1 part2

--- day1.hs
+++ day1.hs
@@ -3,3 +3,6 @@ fuel mass = mass `div` 3 - 2
 
 part1 :: String -> Int
 part1 = sum . map fuel . map read . lines
+
+part2 :: String -> Int
+part2 = sum . map (sum . takeWhile (> 0) . drop 1 . iterate fuel) . map read . lines
