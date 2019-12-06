043d88807ff457062bef89164f80b55ecb0473ba
Nikolai Vavilov <vvnicholas@gmail.com>
2019-12-06 17:35:35 +0200

    Add day2 part2

--- day2.hs
+++ day2.hs
@@ -1,3 +1,4 @@
+import Control.Monad
 import Data.Sequence (Seq(..))
 import qualified Data.Sequence as Seq
 
@@ -18,3 +19,12 @@ run pos program = case Seq.drop pos program of
 
 part1 :: String -> Int
 part1 = run 0 . Seq.update 1 12 . Seq.update 2 2 . parse
+
+part2 :: String -> Int
+part2 = head . good . parse
+  where good program = do
+        noun <- [0..99]
+        verb <- [0..99]
+        let output = run 0 $ Seq.update 1 noun $ Seq.update 2 verb $ program
+        guard $ output == 19690720
+        return $ 100 * noun + verb
