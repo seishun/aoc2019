5225515912bc2887e99082665cf7ec2a13a44fe4
Nikolai Vavilov <vvnicholas@gmail.com>
2019-12-07 13:30:48 +0200

    Add day4 part2

--- day4.hs
+++ day4.hs
@@ -1,12 +1,15 @@
 import Control.Monad
+import Data.List
 
-passwords :: (Int, Int) -> [Int]
-passwords (from, to) = do
-  password <- [from..to]
-  let pairs = zip <*> tail $ show password
-  guard $ any (uncurry (==)) pairs
-  guard $ all (uncurry (<=)) pairs
+passwords :: (Int -> Bool) -> (Int, Int) -> [String]
+passwords crit (from, to) = do
+  password <- show <$> [from..to]
+  guard $ any (crit . length) $ group password
+  guard $ all (uncurry (<=)) $ zip <*> tail $ password
   return password
 
 part1 :: (Int, Int) -> Int
-part1 = length . passwords
+part1 = length . passwords (>= 2)
+
+part2 :: (Int, Int) -> Int
+part2 = length . passwords (== 2)
