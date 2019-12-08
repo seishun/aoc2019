e836c2bcf18f6d73cdc2d78f95cdba34d780fd69
Nikolai Vavilov <vvnicholas@gmail.com>
2019-12-08 13:05:04 +0200

    Add day6 part2

--- day6.hs
+++ day6.hs
@@ -9,3 +9,10 @@ part1 input =
   let (a, b) = unzip $ map parse $ lines input
       m = Map.fromList $ ("COM", 0) : zip a (map (succ . (Map.!) m) b)
   in Map.foldl' (+) 0 m
+
+part2 :: String -> Int
+part2 input =
+  let (a, b) = unzip $ map parse $ lines input
+      m = Map.fromList $ ("COM", []) : zip a (map ((:) <*> (Map.!) m) b)
+      [you, san] = map (reverse . (Map.!) m) ["YOU", "SAN"]
+  in length you + length san - 2 * (length $ takeWhile (uncurry (==)) $ zip you san)
