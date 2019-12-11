82c73fa8aa503c62740a0a3f53f9e61a3330bcaa
Nikolai Vavilov <vvnicholas@gmail.com>
2019-12-11 17:23:05 +0200

    Add day10 part2

--- day10.hs
+++ day10.hs
@@ -1,17 +1,38 @@
 import Data.List
+import Data.Ord
+import Control.Monad
+import qualified Data.Map as Map
 import qualified Data.Set as Set
 
+relative :: (Int, Int) -> (Int, Int) -> (Int, Int)
+relative (x1, y1) (x2, y2) = (x2 - x1, y1 - y2)
+
 angle :: (Int, Int) -> (Int, Int) -> (Int, Int)
-angle (x1, y1) (x2, y2) =
-  let x = x2 - x1
-      y = y2 - y1
+angle a b =
+  let (x, y) = relative a b
   in (x `div` gcd x y, y `div` gcd x y)
 
 parse :: String -> [(Int, Int)]
 parse = concat . map (uncurry line) . zip [0..] . lines
-  where line x = map ((,) x) . elemIndices '#'
+  where line y = map (flip (,) y) . elemIndices '#'
+
+best :: [(Int, Int)] -> (Int, (Int, Int))
+best asteroids =
+  let visible = map (Set.size . Set.fromList . angles) asteroids
+  in maximumBy (comparing fst) $ zip visible asteroids
+  where angles asteroid = map (angle asteroid) $ filter (/= asteroid) asteroids
 
 part1 :: String -> Int
-part1 input = maximum $ map (Set.size . Set.fromList . angles) asteroids
+part1 input = fst $ best asteroids
   where asteroids = parse input
-        angles asteroid = map (angle asteroid) $ filter (/= asteroid) asteroids
+
+part2 :: String -> Int
+part2 input =
+  let (x, y) = snd $ best asteroids
+      (x', y') = (!! 199) $ Map.elems $ Map.fromList $ do
+        asteroid <- asteroids
+        guard $ asteroid /= (x, y)
+        let (x', y') = relative (x, y) asteroid
+        return (atan2 (-fromIntegral x') (-fromIntegral y'), (x', y'))
+      in (x + x') * 100 + (y - y')
+  where asteroids = parse input
