f40ad476261376db05a49a5b192bcc07a43d4d41
Nikolai Vavilov <vvnicholas@gmail.com>
2019-12-14 16:01:54 +0200

    Add day14 part2

--- day14.hs
+++ day14.hs
@@ -1,17 +1,39 @@
 import Data.Map (Map(..))
 import qualified Data.Map as Map
 
-parse :: [String] -> ([(Int, String, Int, String)], (Int, String))
-parse ["=>", ou, oc] = ([], (read ou, oc))
-parse (iu : ic : xs) =
-  let (i, o@(ou, oc)) = parse xs
+type Reaction = (Int, String, Int, String)
+
+parse' :: [String] -> ([Reaction], (Int, String))
+parse' ["=>", ou, oc] = ([], (read ou, oc))
+parse' (iu : ic : xs) =
+  let (i, o@(ou, oc)) = parse' xs
   in ((read iu, ic, ou, oc) : i, o)
 
-part1 :: String -> Int
-part1 input =
-  let reactions = map (fst . parse . words . filter (/= ',')) $ lines input
-      m = Map.fromListWith (+) $ ("FUEL", 1) : map require (concat reactions)
+parse :: String -> [Reaction]
+parse = concatMap (fst . parse' . words . filter (/= ',')) . lines
+
+ore :: Int -> [Reaction] -> Int
+ore fuel reactions =
+  let m = Map.fromListWith (+) $ ("FUEL", fuel) : map require reactions
         where require (iu, ic, ou, oc) =
                 let ceil = (m Map.! oc + ou - 1) `div` ou
                 in (ic, iu * ceil)
   in m Map.! "ORE"
+
+bs' :: Int -> Int -> [Reaction] -> Int
+bs' lo hi reactions
+  | succ lo == hi = lo
+  | ore mid reactions > 1000000000000 = bs' lo mid reactions
+  | otherwise = bs' mid hi reactions
+  where mid = (lo + hi) `div` 2
+
+bs :: Int -> [Reaction] -> Int
+bs fuel reactions
+  | ore (fuel * 2) reactions > 1000000000000 = bs' fuel (fuel * 2) reactions
+  | otherwise = bs (fuel * 2) reactions
+
+part1 :: String -> Int
+part1 = ore 1 . parse
+
+part2 :: String -> Int
+part2 = bs 1 . parse
