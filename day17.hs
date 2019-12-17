8f5808643933e88ea4ca0cf294980e08fd3c363e
Nikolai Vavilov <vvnicholas@gmail.com>
2019-12-17 22:24:48 +0200

    Add day17 part2

--- day17.hs
+++ day17.hs
@@ -1,6 +1,9 @@
+import Control.Applicative
 import Control.Monad
+import Data.List
 import Data.Map (Map(..))
 import qualified Data.Map as Map
+import Data.Maybe
 
 parse :: String -> Map Int Int
 parse input = Map.fromList $ zip [0..] $ read $ "[" ++ input ++ "]"
@@ -39,18 +42,89 @@ run input pos base program =
   where get pos = Map.findWithDefault 0 pos program
         set pos value = Map.insert pos value program
 
-parameters :: [String] -> [Int]
-parameters lines = do
+parameters :: Map (Int, Int) Char -> [Int]
+parameters m = do
+  ((x, y), point) <- Map.toList m
+  guard $ point == '#'
+  guard $ all scaffold [(pred x, y), (succ x, y), (x, pred y), (x, succ y)]
+  return $ x * y
+    where scaffold pos = Map.lookup pos m == Just '#'
+
+left, right :: Char -> Char
+left '^' = '<'
+left '>' = '^'
+left 'v' = '>'
+left '<' = 'v'
+right '^' = '>'
+right '>' = 'v'
+right 'v' = '<'
+right '<' = '^'
+
+move :: (Int, Int) -> Char -> (Int, Int)
+move (x, y) '^' = (x, pred y)
+move (x, y) '>' = (succ x, y)
+move (x, y) 'v' = (x, succ y)
+move (x, y) '<' = (pred x, y)
+
+data Move = TL | TR | MF Int deriving (Eq)
+instance Show Move where
+  show TL = "L"
+  show TR = "R"
+  show (MF units) = show units
+
+view :: [String] -> Map (Int, Int) Char
+view lines = Map.fromList $ do
   (y, line) <- zip [0..] lines
   (x, point) <- zip [0..] line
-  guard $ point == '#'
-  guard $ y > 0 && y < length lines - 1
-  guard $ x > 0 && x < length line - 1
-  guard $ line !! pred x == '#'
-  guard $ line !! succ x == '#'
-  guard $ lines !! pred y !! x == '#'
-  guard $ lines !! succ y !! x == '#'
-  return $ x * y
+  return ((x, y), point)
+
+path :: (Int, Int) -> Char -> Map (Int, Int) Char -> [Move]
+path pos dir0 m = fromMaybe [] $
+    go dir0 id <|>
+    go (left dir0) (TL :) <|>
+    go (right dir0) (TR :)
+  where
+    go dir f =
+      let next = move pos dir
+      in case Map.findWithDefault '.' next m of
+        '#' -> Just (f $ MF 1 : path next dir m)
+        '.' -> Nothing
+
+pathify :: [Move] -> [Move]
+pathify moves = map p $ group moves
+  where p [TR] = TR
+        p [TL] = TL
+        p l = MF $ length l
+
+collapse :: [Move] -> [[Move]] -> [([Int], [[Move]])]
+collapse [] fs = [([], fs)]
+collapse moves fs =
+  let splits = zip (tail $ inits moves) (tail $ tails moves)
+      matches = mapMaybe (\(i, t) -> ((,) t) <$> elemIndex i fs) splits
+  in if null matches then
+    if length fs == 3 then []
+    else do
+      (i, t) <- filter (\(i, t) -> isInfixOf i t) splits
+      (is, fs') <- collapse t (fs ++ [i])
+      return (length fs : is, fs')
+  else do
+      (t, i) <- matches
+      (is, fs') <- collapse t fs
+      return (i : is, fs')
 
 part1 :: String -> Int
-part1 = sum . parameters . lines . map toEnum . run [] 0 0 . parse
+part1 = sum . parameters . view . lines . map toEnum . run [] 0 0 . parse
+
+part2 :: String -> Int
+part2 input =
+  let program = parse input
+      m = view $ lines $ map toEnum $ run [] 0 0 program
+      (pos, dir) = head $ Map.toList $ Map.filter (`elem` "^>v<") m
+      i = intercalate "\n" $ head $ do
+        (i, fs) <- collapse (pathify $ path pos dir m) []
+        let i' = intersperse ',' $ map ("ABC" !!) i
+        guard $ length i' <= 20
+        let fs' = map (intercalate "," . map show) fs
+        guard $ all ((<= 20) . length) fs'
+        return $ i' : fs'
+  in last $ run (map fromEnum $ i ++ "\nn\n") 0 0 $ Map.insert 0 2 program
